c7bd4733bfc16d42e4f6b9a99f839d18b510d8fe
Stefan Reinhold <sre@informatik.uni-kiel.de>
2019-03-18 14:22:12 +0100

    Parameter optimization

--- toolbox/+microCT/meshCortex.m
+++ toolbox/+microCT/meshCortex.m
@@ -42,7 +42,7 @@ oldPath = addpath(sprintf('%s/iso2mesh/', rootDir));
 % Mesh volume
 r = max(size(cortexCenterDist))^3 / 2;
 c = size(cortexCenterDist) / 2;
-[V, F] = vol2restrictedtri(double(cortexCenterDist), -1e-9, c, r, 30, 2, 2, 50000);
+[V, F] = vol2restrictedtri(double(cortexCenterDist), -1e-9, c, r, 30, 5, 0.1, 100000);
 V = single(V);
 if length(V) <= intmax('uint16')
   F = uint16(F);
